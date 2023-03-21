980f0c8861d10c7964ace9aa7d164f54b4091307
Adam Young <adamyoungy678@gmail.com>
2023-03-21 13:47:42 +0000

    iterating arrays practice setup with while loop

---
+++ iterating.php
@@ -0,0 +1,16 @@
+<?php
+echo '<h1>Iterating Through Arrays</h1>';
+
+$days = [
+    'Mon',
+    'Tues',
+    'Wed',
+    'Thu',
+    'Fri',
+    'Sat',
+    'Sun'
+];
+
+echo '<h2>While Loop</h2>';
+
+$daysArrayLength = count($days);
