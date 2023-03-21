63ee3f15cfe7a9573d3dbc10b54d1a729a79d80c
Adam Young <adamyoungy678@gmail.com>
2023-03-21 15:14:55 +0000

    sorted the array names

--- exercise.php
+++ exercise.php
@@ -1,6 +1,6 @@
 <?php
 $trainees = ['adam', 'brent', 'michael', 'anna', 'dom', 'henry', 'james', 'phil'];
-
+sort($trainees);
 foreach($trainees as $trainee) {
     echo '<p>' . $trainee . '</p>';
 }
