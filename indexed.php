9be744ae4a0ad60f38b26385441daf395225d8e5
Adam Young <adamyoungy678@gmail.com>
2023-03-21 11:54:29 +0000

    use column format for longer arrays for easier readability

--- indexed.php
+++ indexed.php
@@ -1,8 +1,16 @@
 <?php
 echo '<h1>Indexed Arrays</h1>';
 
-$days = ['Mon', 'Tues', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+$days = [
+    'Mon',
+    'Tues',
+    'Wed',
+    'Thu',
+    'Fri',
+    'Sat',
+    'Sun'
+];
 
-echo '<pre>';
+//echo '<pre>';
 print_r($days);
-echo '</pre>';
+//echo '</pre>';
