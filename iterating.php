f90aade6ab47f5a617a2ce4679a11a324fc89b8b
Adam Young <adamyoungy678@gmail.com>
2023-03-21 14:02:35 +0000

    using a for loop

--- iterating.php
+++ iterating.php
@@ -17,6 +17,12 @@ $daysArrayLength = count($days);
 
 $i = 0;
 while ($i < $daysArrayLength) {
-    echo '<p>' .$days[$i] . '</p>';
+    echo '<p>' . $days[$i] . '</p>';
     $i++;
+}
+
+echo '<h2>For Loop</h2>';
+
+for ($i = 0; $i < $daysArrayLength; $i++) {
+    echo '<p>' . $days[$i] . '</p>';
 }
