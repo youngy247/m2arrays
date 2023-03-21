f2d038cf0eb4e8fd80ff6a2395b30d5208a43c3f
Adam Young <adamyoungy678@gmail.com>
2023-03-21 14:28:09 +0000

    using modulus operator to output only even numbers of an array

--- iterating.php
+++ iterating.php
@@ -50,4 +50,11 @@ foreach ($personalDetails as $key => $detail) {
     echo '<p>' . $key . 'is ' . $detail . '</p>';
     }
 
+$nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+foreach($nums as $num) {
+    if ($num % 2 === 0) {
+        echo '<p>' . $num . '</p>';
+    }
+}
 
