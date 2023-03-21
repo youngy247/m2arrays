c31b1b3d99d1e63ade339ce6b4b4d65e53f10a5d
Adam Young <adamyoungy678@gmail.com>
2023-03-21 15:44:27 +0000

    shuffled and output the even numbers in an array and the same with odd numbers

--- exercise.php
+++ exercise.php
@@ -1,6 +1,25 @@
 <?php
-$trainees = ['adam', 'brent', 'michael', 'anna', 'dom', 'henry', 'james', 'phil'];
-sort($trainees);
-foreach($trainees as $trainee) {
-    echo '<p>' . $trainee . '</p>';
+//$trainees = ['adam', 'brent', 'michael', 'anna', 'dom', 'henry', 'james', 'phil'];
+//sort($trainees);
+//foreach($trainees as $trainee) {
+//    echo '<p>' . $trainee . '</p>';
+//}
+
+$nums = [1, 2, 3, 4, 5, 6, 7, 8];
+$nums[] = 9;
+
+array_push($nums, 10);
+
+shuffle($nums);
+
+foreach($nums as $num) {
+    if ($num % 2 === 0) {
+        echo '<p>' . $num . '</p>';
+    }
+}
+
+foreach($nums as $num) {
+    if ($num % 2 === 1) {
+        echo '<p>' . $num . '</p>';
+    }
 }
